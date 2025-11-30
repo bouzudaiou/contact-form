d90ba23392fb3471e81fb21ba37e11ed26a100fd
yoshinobu k <ysnb5884@gmail.com>
2025-11-30 11:50:41 +0900

    Factoryの実装

---
+++ src/database/factories/ContactFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Contact;
+
+class ContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->safeEmail,
+            'tel' => $this->faker->numerify('###########'),
+            'content' => $this->faker->realText(100),
+        ];
+    }
+}
